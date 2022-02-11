170660d18e52d1c10ee935e1eeef549ada84f14f
Justin Vongdara <Vongdara16@gmail.com>
2022-02-11 11:42:02 -0500

    change html. no id

--- js/app.js
+++ js/app.js
@@ -14,12 +14,21 @@ const theme = document.querySelector('.dropdown-item')
 
 /*----------------------------- Event Listeners -----------------------------*/
 document.querySelector('#play-btn').addEventListener('click', init)
+document.querySelector('#start-menu-btn').addEventListener('click', init)
+document.querySelector('#reset-btn').addEventListener('click', init)
 
 
 /*-------------------------------- Functions --------------------------------*/
 
 function init(){
-  console.log(playerName1.value)
-  console.log(playerName2.value)
-
+  // console.log(playerName1.value)
+  // console.log(playerName2.value)
+  turn = 1
+  winner = null
+  boardArray = [
+    [1, 1, 1, null, null, -1, -1, -1],  // array 0
+    [1, 1, 1, null, null, -1, -1, -1],  // array 1
+    [1, 1, 1, null, null, -1, -1, -1],  // array 2
+    [1, 1, 1, null, null, -1, -1, -1]   // array 3
+  ]
 }
