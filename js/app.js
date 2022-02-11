582317a5f405ace72dea8cbc1e32a06f4326cb5e
Justin Vongdara <Vongdara16@gmail.com>
2022-02-11 11:13:47 -0500

    test player name input

--- js/app.js
+++ js/app.js
@@ -7,14 +7,19 @@ let turn, winner, boardArray
 
 
 /*------------------------ Cached Element References ------------------------*/
-
+const playerName1 = document.querySelector('#input-player1')
+const playerName2 = document.querySelector('#input-player2')
+const theme = document.querySelector('.dropdown-item')
 
 
 /*----------------------------- Event Listeners -----------------------------*/
-
+document.querySelector('#play-btn').addEventListener('click', init)
 
 
 /*-------------------------------- Functions --------------------------------*/
 
+function init(){
+  console.log(playerName1.value)
+  console.log(playerName2.value)
 
-
+}
